e91a614d93947c6ef79131367562c3f65e793ff2
Ben Plumley <benplumley@gmail.com>
2015-11-16 13:55:11 +0000

    Window setup code

--- Desktop-Client/Client.java
+++ Desktop-Client/Client.java
@@ -1,7 +1,50 @@
-public class Client {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class Client implements Runnable {
+
+	String hostname = "localhost";
+	String portNumber = "55511";
 
 	public static void main(String[] args) {
-		
+		Client client = new Client();
+	}
+
+	public void Client() {
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	public void run() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			// matches the operating system's look and feel
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {}
+		setupPanels();
+		setupFrame();
+	}
+
+	private void setupFrame() {
+		JFrame window = new JFrame("Car Sharing");
+		window.setIconImage(Toolkit.getDefaultToolkit().getImage("graphics/icon.png"));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		populateFrame(window.getContentPane());
+		window.pack();
+		window.setLocationByPlatform(true);
+		window.setVisible(true);
+		Dimension windowSize = new Dimension(400, 200);
+		window.setSize(windowSize);
+		window.setResizable(true);
+	}
+
+	private void setupPanels() {
+
+	}
+
+	private void populateFrame(Container frame) {
+
 	}
 
 }
