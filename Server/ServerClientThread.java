4e3ecc58641e4c3b714d14cd76543b200c56bb12
Arthur <al687@bath.ac.uk>
2015-11-22 13:37:38 +0000

    Server client thread
    Not sure if this is the best way to do this, but it’s ready.

---
+++ Server/ServerClientThread.java
@@ -0,0 +1,10 @@
+/* Integrated Project 2015/16
+ * University of Bath
+ *
+ * This class is supposed to present a user on the server
+ * side of things.
+ */
+
+public class ServerClientThread implements Runnable {
+
+}
