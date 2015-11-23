7d808e2c0ba688e6a268fd15afbd110be4a7ad11
Arthur <al687@bath.ac.uk>
2015-11-23 14:10:45 +0000

    Nothing important

--- Server/ServerClientThread.java
+++ Server/ServerClientThread.java
@@ -1,15 +1,14 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-
 /* Integrated Project 2015/16
  * University of Bath
  *
  * This class is supposed to present a user on the server
  * side of things.
  */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class ServerClientThread implements Runnable {
 	private Socket myServerSocket;
