5f276770066266b1f41d94c9307a3ea10c2ad20a
Ben Plumley <benplumley@gmail.com>
2016-03-24 16:07:27 +0000

    Commented code out to compile

--- Desktop-Client/Ride.java
+++ Desktop-Client/Ride.java
@@ -2,19 +2,19 @@ public class Ride {
 
 	private MapPoint startPoint;
 	private MapPoint endPoint;
-	private DateTime dateTime = new DateTime();
+	// private DateTime dateTime = new DateTime();
 	private char[] repeatingDays;
 	private byte numberOfSeats;
 	private byte seatsRemaining;
 
-	public Ride(MapPoint startPoint, MapPoint endPoint, DateTime dateTime, char[] repeatingDays, byte numberOfSeats, byte seatsRemaining) {
-		startPoint = this.startPoint;
-		endPoint = this.endPoint;
-		dateTime = this.dateTime;
-		repeatingDays = this.repeatingDays;
-		numberOfSeats = this.numberOfSeats;
-		seatsRemaining = this.seatsRemaining;
-	}
+	// public Ride(MapPoint startPoint, MapPoint endPoint, DateTime dateTime, char[] repeatingDays, byte numberOfSeats, byte seatsRemaining) {
+	// 	startPoint = this.startPoint;
+	// 	endPoint = this.endPoint;
+	// 	dateTime = this.dateTime;
+	// 	repeatingDays = this.repeatingDays;
+	// 	numberOfSeats = this.numberOfSeats;
+	// 	seatsRemaining = this.seatsRemaining;
+	// }
 
 
 }
