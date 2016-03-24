3726308ec94d2d6c13e1630f6bffa01ff63e71c6
Ben Plumley <benplumley@gmail.com>
2016-03-24 18:38:00 +0000

    Fix ride class

--- Desktop-Client/Ride.java
+++ Desktop-Client/Ride.java
@@ -1,20 +1,22 @@
-public class Ride {
+import java.sql.Date;
+
+public class Ride implements java.io.Serializable {
 
 	private MapPoint startPoint;
 	private MapPoint endPoint;
-	// private DateTime dateTime = new DateTime();
+	private Date dateTime;
 	private char[] repeatingDays;
 	private byte numberOfSeats;
 	private byte seatsRemaining;
 
-	// public Ride(MapPoint startPoint, MapPoint endPoint, DateTime dateTime, char[] repeatingDays, byte numberOfSeats, byte seatsRemaining) {
-	// 	startPoint = this.startPoint;
-	// 	endPoint = this.endPoint;
-	// 	dateTime = this.dateTime;
-	// 	repeatingDays = this.repeatingDays;
-	// 	numberOfSeats = this.numberOfSeats;
-	// 	seatsRemaining = this.seatsRemaining;
-	// }
+	public Ride(MapPoint startPoint, MapPoint endPoint, Date dateTime, char[] repeatingDays, byte numberOfSeats, byte seatsRemaining) {
+		startPoint = this.startPoint;
+		endPoint = this.endPoint;
+		dateTime = this.dateTime;
+		repeatingDays = this.repeatingDays;
+		numberOfSeats = this.numberOfSeats;
+		seatsRemaining = this.seatsRemaining;
+	}
 
 
 }
