c8f17736fd5eaa85b04d4bc28ba49013b8d49ef1
Ben Plumley <benplumley@gmail.com>
2016-03-25 15:12:46 +0000

    basic datetime functionality

--- Desktop-Client/DateTime.java
+++ Desktop-Client/DateTime.java
@@ -1,5 +1,21 @@
 //For our own implementation of date and times
 
-public class DateTime {
+import java.text.DateFormat;
+
+public class DateTime implements java.io.Serializable {
+
+    long epoch = 0;
+
+    public DateTime(long epoch) {
+        this.epoch = epoch;
+    }
+
+    public String dateString() {
+        return DateFormat.getDateInstance().format(epoch);
+    }
+
+    public String timeString() {
+        return DateFormat.getTimeInstance().format(epoch);
+    }
 
 }
