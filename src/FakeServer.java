1b827adfd192cc93b465f4a118136b500a2f0a0c
Ben Plumley <benplumley@gmail.com>
2016-03-28 18:25:55 +0100

    finish fake server

--- src/FakeServer.java
+++ src/FakeServer.java
@@ -9,11 +9,19 @@ public class FakeServer {
     public Ride[] getMatchingRides(Boolean isToUni, DateTime dateAndTime, int timeTolerance) {
         byte zero = 0;
         byte four = 4;
-        Ride[] result = {new Ride(true, new MapPoint(31,76), new DateTime(1459183241L),zero,four,four),
-                         new Ride(true, new MapPoint(97,181), new DateTime(1459189641L),zero,four,four),
-                         new Ride(false, new MapPoint(305,150), new DateTime(1459189641L),zero,four,four),
-                         new Ride(false, new MapPoint(720,300), new DateTime(1459189641L), zero,four,four)};
-        return result;
+        if (isToUni) {
+            Ride[] result = {new Ride(true, new MapPoint(30,85), new DateTime(1459183241L),zero,four,four),
+                             new Ride(true, new MapPoint(35,85), new DateTime(1459189641L),zero,four,four),
+                             new Ride(true, new MapPoint(305,150), new DateTime(1459189641L),zero,four,four),
+                             new Ride(true, new MapPoint(720,300), new DateTime(1459189641L), zero,four,four)};
+            return result;
+        } else {
+            Ride[] result = {new Ride(false, new MapPoint(52,99), new DateTime(1459183241L),zero,four,four),
+                             new Ride(false, new MapPoint(253,102), new DateTime(1459189641L),zero,four,four),
+                             new Ride(false, new MapPoint(652,400), new DateTime(1459189641L),zero,four,four),
+                             new Ride(false, new MapPoint(670,48), new DateTime(1459189641L), zero,four,four)};
+            return result;
+        }
     }
 
     public Boolean book(Ride ride) {
