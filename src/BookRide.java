5f86e73f3793b654efc170b66a5637f5265720ea
Ben Plumley <benplumley@gmail.com>
2016-04-16 00:00:07 +0100

    fix merge

--- src/BookRide.java
+++ src/BookRide.java
@@ -1,13 +1,14 @@
 public class BookRide extends ClientToServer {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 4109739758499069884L;
-	
-	public BookRide() {
-		super(ClientToServerPurpose.RIDE_BOOKING);
-	}
+	private int UUID;
 
+	public BookRide(int UUID) {
+		super(ClientToServerPurpose.RIDE_BOOKING);
+		this.UUID = UUID;
+	}
 
 }
--- src/NewRide.java
+++ src/NewRide.java
@@ -2,10 +2,11 @@
 public class NewRide extends ClientToServer {
 
 	private static final long serialVersionUID = -8586202014045591404L;
+	private Ride ride;
 
-	public NewRide() {
+	public NewRide(Ride ride) {
 		super(ClientToServerPurpose.NEW_RIDE);
+		this.ride = ride;
 	}
-
 
 }
