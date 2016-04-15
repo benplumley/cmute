c79360f504f5627196e9189e6f6ae70778343a98
Ben Plumley <benplumley@gmail.com>
2016-04-16 00:16:37 +0100

    map image might work now

--- src/Map.java
+++ src/Map.java
@@ -6,9 +6,10 @@ import java.io.IOException;
 public class Map extends JLabel {
 
 	public Map() {
-		String filePath = "/Users/Artlac/GitHub/IntegratedProject/src/maps/bath.png";
+		String filePath = System.getProperty("user.dir") + "/maps/bath.png";
 //				"maps/bath.png";
-		
+// "/Users/Artlac/GitHub/IntegratedProject/src/maps/bath.png"
+
 		try {
 			this.setIcon(new ImageIcon(ImageIO.read(new File(filePath))));
 		} catch (IOException e) {
