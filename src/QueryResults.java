f8b032c089f2bab9af31dbdbab77f44c1b861cb6
Arthur <al687@bath.ac.uk>
2016-04-17 14:25:24 +0100

    Change of RS

--- src/QueryResults.java
+++ src/QueryResults.java
@@ -2,19 +2,30 @@ import java.sql.ResultSet;
 
 
 public class QueryResults extends ServerToClient {
+	private Ride[] myRides;
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1391915633649891722L;
 
 	public QueryResults(ResultSet rs) {
 		super();
+        while (rs.next()) {
+            String coffeeName = rs.getString("COF_NAME");
+            int supplierID = rs.getInt("SUP_ID");
+            float price = rs.getFloat("PRICE");
+            int sales = rs.getInt("SALES");
+            int total = rs.getInt("TOTAL");
+            System.out.println(coffeeName + "\t" + supplierID +
+                               "\t" + price + "\t" + sales +
+                               "\t" + total);
+        }
 	}
 
+	private void addRide(Ride rideToAdd){
+//		myRides.
+	}
+	
 	public Ride[] getRides() {
-		// TODO Auto-generated method stub
-		return null;
+		return myRides;
 	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -162,17 +162,6 @@ public class Server {
 	        stmt = connection.createStatement();
 	        ResultSet rs = stmt.executeQuery(queryString);
 	        
-	        while (rs.next()) {
-	            String coffeeName = rs.getString("COF_NAME");
-	            int supplierID = rs.getInt("SUP_ID");
-	            float price = rs.getFloat("PRICE");
-	            int sales = rs.getInt("SALES");
-	            int total = rs.getInt("TOTAL");
-	            System.out.println(coffeeName + "\t" + supplierID +
-	                               "\t" + price + "\t" + sales +
-	                               "\t" + total);
-	        }
-	        
 	        if (stmt != null) { stmt.close(); }
 
 	        return new QueryResults(rs);
